675e957bdb4312a8ec016b6237359e7f9f5d480b
David Chanel <david.alexandre.chanel.ext@gmail.com>
2015-03-13 18:38:09 +0100

    Added Meld for git diff for OSX with brew

--- shell/setupGit.sh
+++ shell/setupGit.sh
@@ -15,7 +15,20 @@ git config --global push.default "matching"
 
 git config --global color.status auto
 git config --global color.branch auto
-git config --global core.editor vim 
+git config --global core.editor vim
+mkdir -p ~/.git
+case `uname` in
+
+  Darwin)
+        MELD_PATH=/usr/local/Cellar/meld/1.8.6/bin/meld
+	;;
+
+  Linux)
+        MELD_PATH=/usr/bin/local/meld
+        ;;
+esac
+echo "#!/usr/bin/python\nimport sys\nimport os\nos.system('$MELD_PATH "%s" "%s"' % (sys.argv[2], sys.argv[5]))" > ~/.git/git-diff.py
+git config --global diff.external ~/.git/git-diff.py
 git config --global merge.tool meld
 
 # aliases
